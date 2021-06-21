5775b417ff2ae9790d32690dd8dd02bf74843c55
JasonChan <fakeacone1@gmail.com>
2021-06-22 02:58:09 +0800

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const itemList = document.getElementsByClassName('todo')[0];
 let time = new Date();
 let items = JSON.parse(localStorage.getItem('items')) || [];
+
 document.querySelector('#todoForm').addEventListener('submit',(event)=>{
     event.preventDefault();
     const form = event.target;
@@ -11,32 +12,36 @@ document.querySelector('#todoForm').addEventListener('submit',(event)=>{
 })
 
 function addItem(content,dateTime,id){
+    //add item to localStorage and refresh the page
     saveItem(content,dateTime,id);
     itemList.innerHTML="";
     getItem();
 }
 
 function deleteItem(event){
+    //delete the todo item
     const btn = event.currentTarget;
     const str_id = btn.id.replace("del_", "");
     const id = parseInt(str_id);
     for(let i=0;i<items.length;i++){
         if(items[i].id==id){
-            console.log(items[i]);
             items.splice(i,1);
         }
     }
+    //update localStorage and refresh page
     localStorage.setItem('items',JSON.stringify(items));
     itemList.innerHTML="";
     getItem();
 }
 
 function saveItem(content,dateTime,id){
+    //using localStorage to store the content
     items.push({content:content,time:dateTime,id:id});
     localStorage.setItem('items',JSON.stringify(items));
 }
 
 function getItem(){
+    //take all the item in items then display it
     for(let item of items){
     itemList.innerHTML+=`
     <div class="item" id="item${item.id}">
